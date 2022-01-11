daff848171c1d87d936a300fdede8af234487dcc
pluto168 <pluto88.cloud@gmail.com>
2022-01-11 20:39:21 +0800

    chang name

--- 可迭代對象.js
+++ 可迭代對象.js
@@ -2,12 +2,13 @@ class Player {
   constructor(list) {
     this.list = list;
   }
+  //迭代器
   [Symbol.iterator]() {
     let current = 0;
     let that = this;
     return {
       next() {
-        return current < that.list.lenght
+        return current < that.list.length
           ? { value: that.list[current++], done: false }
           : { done: true };
       },
