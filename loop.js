74fd539becf776d26644b3552ff619f8d3e2189c
pluto168 <pluto88.cloud@gmail.com>
2022-01-17 15:39:03 +0800

    add loop

--- loop.js
+++ loop.js
@@ -1,3 +1,26 @@
-for (let rep = 0; rep <= 10; rep++) {
-  const element = rep;
+//// loop
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition${rep}`);
+// }
+
+//script
+const jonasArray = [
+  "Jonas",
+  "Schmedtmann",
+  2037 - 1991,
+  "teacher",
+  ["lin", "lee", "chen"],
+  true,
+];
+const types = [];
+
+for (let i = 0; i < jonasArray.length; i++) {
+  //Reading types array
+  console.log(jonasArray[i], typeof jonasArray[i]);
+
+  //Filling types array
+  //   types[i] = typeof jonasArray[i];
+  types.push(typeof jonasArray[i]);
 }
+
+console.log(types);
