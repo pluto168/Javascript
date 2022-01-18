784b5180d15deb97e4cf750a292ccd10a07316ca
pluto168 <pluto88.cloud@gmail.com>
2022-01-18 18:06:26 +0800

    add

--- loop.js
+++ loop.js
@@ -63,3 +63,59 @@
 //   return sum / arr.length;
 // };
 // console.log(calcAverage([1, 2, 3]));
+
+//temperaturesNew calculate Problem:
+// const temperaturesNew = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+//   console.log(temps);
+
+//   let max = 0;
+//   let min = 0;
+
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   console.log(max, min);
+//   return max - min;
+// };
+
+// const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 4, 5]);
+// console.log(amplitudeNew);
+
+// //Problem 2:
+// //function should now receive 2 array of temps
+// //Merge 2 arrays
+
+// const measureKelvin = function () {
+//   const measurement = {
+//     type: 'temp',
+//     unit: 'cels',
+//     value: Number(prompt('Degrees celsius:')),
+//   };
+//   console.table(measurement);
+
+//   const kelvin = measurement.value + 273;
+//   return kelvin;
+// };
+
+// console.log(measureKelvin());
+
+//1) Understanding the problem:
+
+// const data1 = [17, 21, 23];
+// const data2 = [12, 5, -5, 0, 4];
+
+// console.log(`...${data1[0]}C...${data1[1]}C...${data1[2]}C...`);
+
+// const printForecast = function (arr) {
+//   let str = '';
+//   for (let i = 0; i < arr.length; i++) {
+//     str += `${arr[i]}C in ${i + 1}day ... `;
+//   }
+//   console.log('...' + str);
+// };
+// printForecast(data1);
